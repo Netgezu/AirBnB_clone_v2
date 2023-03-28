97a4fee681204ed6eb499ef386bd626a37c9f6d9
Netgezu <106767573+Netgezu@users.noreply.github.com>
2023-03-28 08:56:13 +0300

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,73 +1,50 @@
 #!/usr/bin/python3
-"""test for console"""
+""" Unittests : console """
 import unittest
-from unittest.mock import patch
+from console import HBNBCommand
 from io import StringIO
+import json
+import sys
+from unittest.mock import patch
 import pep8
 import os
-import json
-import console
-import tests
-from console import HBNBCommand
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.engine.file_storage import FileStorage
 
 
 class TestConsole(unittest.TestCase):
-    """this will test the console"""
+    """ test console class """
 
-    @classmethod
-    def setUpClass(cls):
-        """setup for the test"""
-        cls.consol = HBNBCommand()
-
-    @classmethod
-    def teardown(cls):
-        """at the end of the test this will tear it down"""
-        del cls.consol
-
-    def tearDown(self):
-        """Remove temporary file (file.json) created as a result"""
-        try:
-            os.remove("file.json")
-        except Exception:
-            pass
-
-    def test_pep8_console(self):
-        """Pep8 console.py"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(["console.py"])
-        self.assertEqual(p.total_errors, 0, 'fix Pep8')
-
-    def test_docstrings_in_console(self):
-        """checking for docstrings"""
-        self.assertIsNotNone(console.__doc__)
-        self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_EOF.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_create.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_show.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_destroy.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_all.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_update.__doc__)
-        self.assertIsNotNone(HBNBCommand.count.__doc__)
-        self.assertIsNotNone(HBNBCommand.strip_clean.__doc__)
-        self.assertIsNotNone(HBNBCommand.default.__doc__)
-
-    def test_emptyline(self):
-        """Test empty line input"""
+    def test_all(self):
+        """ test all method """
         with patch('sys.stdout', new=StringIO()) as f:
-            self.consol.onecmd("\n")
-            self.assertEqual('', f.getvalue())
+            HBNBCommand().onecmd("all NUGGET")
+            self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
 
-    def test_quit(self):
-        """test quit command inpout"""
+    def test_show(self):
+        """ test show method """
         with patch('sys.stdout', new=StringIO()) as f:
-            self.consol.onecmd("quit")
-            self.assertEqual('', f.getvalue())
+            HBNBCommand().onecmd("show NUGGET")
+            self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
+
+    def test_create(self):
+        """ test create method """
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create NUGGET")
+            self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
+
+    def test_update(self):
+        """ test update method """
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("update NUGGET")
+            self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
+
+    def test_destroy(self):
+        """ test destroy method """
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("destroy NUGGET")
+            self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
+
+    def test_destroy(self):
+        """ test detroy method """
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("destroy NUGGET")
+            self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
